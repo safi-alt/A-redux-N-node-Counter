bbbccbea79d843216d47372f36bccdd8ba9f1a71
safi <safiazim8@gmail.com>
2020-07-08 21:23:30 +0500

    Dispatching action from within the Component

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -40,7 +40,7 @@ class Counter extends Component {
         <CounterOutput value={this.props.ctr} />
         <CounterControl
           label="Increment"
-          clicked={() => this.counterChangedHandler("inc")}
+          clicked={this.props.onIncrementCounter}
         />
         <CounterControl
           label="Decrement"
@@ -65,4 +65,10 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps)(Counter);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onIncrementCounter: () => dispatch({ type: "INCREMENT" }),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
