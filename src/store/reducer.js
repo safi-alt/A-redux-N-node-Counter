e59980daa924a3092ac140e3827831ab6c0ae64f
safi <safiazim8@gmail.com>
2020-07-09 20:26:22 +0500

    Passing and Retrieving Data with Action

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -8,6 +8,22 @@ const reducer = (state = initialState, action) => {
       counter: state.counter + 1,
     };
   }
+  if (action.type === "DECREMENT") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+  if (action.type === "ADD") {
+    return {
+      counter: state.counter + action.value,
+    };
+  }
+  if (action.type === "SUBTRACT") {
+    return {
+      counter: state.counter - action.value,
+    };
+  }
+
   return state;
 };
 
