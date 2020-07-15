cbb40d2dd3e6b490d06c4a652535d6d73ea0063b
safi <safiazim8@gmail.com>
2020-07-16 00:53:06 +0500

    A Leaner Switch and Case Statement

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -5,6 +5,13 @@ const initialState = {
   results: [],
 };
 
+const deleteResult = (state, action) => {
+  const updatedArray = state.results.filter(
+    (result) => result.id !== action.resultElId
+  );
+  return updateObject(state, { results: updatedArray });
+};
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.STORE_RESULT:
@@ -20,10 +27,7 @@ const reducer = (state = initialState, action) => {
       // const id = 2;
       // const newArray = [...state.results];
       // newArray.splice(id, 1);
-      const updatedArray = state.results.filter(
-        (result) => result.id !== action.resultElId
-      );
-      return updateObject(state, { results: updatedArray });
+      return deleteResult(state, action);
   }
 
   // if (action.type === "INCREMENT") {
